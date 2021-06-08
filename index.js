437dc7e9419e8bab826382dd2cc5e28a40c73beb
Rick <rmansfield@realpeoplesratings.com>
2021-06-08 18:57:09 -0500

    complete task 5b feet to cm

--- index.js
+++ index.js
@@ -219,9 +219,10 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+    return cm/30.48;
   }
+  console.log('task 5b', feet(3));
  
 
 
